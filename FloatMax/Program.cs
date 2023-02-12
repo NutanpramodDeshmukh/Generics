469f24bc58d276c8a2350313d6dade57da9085e8
NutanpramodDeshmukh <nutanpd17@gmail.com>
2023-02-12 21:56:02 +0530

    Added:Float max

---
+++ FloatMax/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatMax
+{
+    internal class Program
+    {
+        public static float floatMaxnum(float firstValue, float secondValue, float thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            return firstValue;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Maximum number = {Program.floatMaxnum(10.1f, 50.6f, 40.9f)}");
+        }
+    }
+}
